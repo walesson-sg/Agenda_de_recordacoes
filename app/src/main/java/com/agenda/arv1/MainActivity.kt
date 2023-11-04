b360834d9195e673d32c8e7c4623bd84d2a8bcf7
WalessonSG <walesson.sg@gmail.com>
2023-11-04 19:21:02 -0300

    adicionado button para ir às mídias da memória

--- app/src/main/java/com/agenda/arv1/MainActivity.kt
+++ app/src/main/java/com/agenda/arv1/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.agenda.arv1.BitmapHelper
+import com.agenda.arv1.MarkerInfoAdapter
 import com.agenda.arv1.PontoRecordacao
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -18,8 +20,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     var rioBV = LatLng(-5.119807554904492, -39.73120742055659)
     var autoVip = LatLng(-7.0209901,-37.2717041)
     private val pontosRecordacao = arrayListOf(
-        PontoRecordacao(1, "BV", "BOA VIAGEM - CE", boaViagem),
-        PontoRecordacao(2, "Forum", "BOA VIAGEM - CE", rioBV),
+        PontoRecordacao(1, "BV", "BOA VIAGEM - CE adsssssssssssssssssssssssssssssssssss", boaViagem),
+        PontoRecordacao(2, "Forum", "BOA VIAGEM - CE asddddddddddddddddddddddddddddddddddd", rioBV),
         PontoRecordacao(3, "AutoVIP", "PARAIBA - CE", autoVip)
     )
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,14 +30,24 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync{googleMap ->
+            googleMap.setInfoWindowAdapter(MarkerInfoAdapter(this))
             onMapReady(googleMap)
+
+            googleMap.setOnMapLoadedCallback {
+                val bounds = LatLngBounds.builder()
+
+                pontosRecordacao.forEach{
+                    bounds.include(it.latLng)
+                }
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 100))
+            }
         }
 
     }
 //-4.9885319,-39.9284245
     override fun onMapReady(googleMap: GoogleMap) {
 //        mGoogleMap = googleMap
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(boaViagem))
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(boaViagem))
 //        googleMap.addMarker(MarkerOptions().position(boaViagem).title("Boa Viagem - CE"))
 
     pontosRecordacao.forEach{memoria->
@@ -48,6 +60,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                     this, R.drawable.baseline_local_cafe_24,
                     ContextCompat.getColor(this, androidx.appcompat.R.color.primary_material_dark)))
         )
+        marker?.tag = memoria
     }
 
     }
---
+++ app/src/main/java/com/agenda/arv1/MarkerInfoAdapter.kt
@@ -0,0 +1,24 @@
+package com.agenda.arv1
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import com.example.arv1.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+
+class MarkerInfoAdapter(private val context : Context) : GoogleMap.InfoWindowAdapter {
+    override fun getInfoContents(marker : Marker): View? = null
+
+    override fun getInfoWindow(marker : Marker): View? {
+        val memoria = marker.tag as? PontoRecordacao ?: return null
+
+        val view = LayoutInflater.from(context).inflate(R.layout.custom_marker_recordacao, null)
+
+        view.findViewById<TextView>(R.id.txt_tittle).text = memoria.nome
+        view.findViewById<TextView>(R.id.txt_description).text = memoria.descricao
+
+        return view
+    }
+}
